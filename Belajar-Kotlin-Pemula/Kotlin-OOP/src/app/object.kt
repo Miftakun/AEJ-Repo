6216f44f94fb60805b3e18778a2bf9a88ea9df73
Miftakun Niam <miftakhunniam999@gmail.com>
2024-02-02 16:40:12 +0700

    Merge remote-tracking branch 'origin/master'

---
+++ Belajar-Kotlin-Pemula/Kotlin-OOP/src/app/object.kt
@@ -0,0 +1,13 @@
+package src.app
+
+import src.data.Person
+
+fun main() {
+    val eko = Person()
+    val joko = Person()
+    val budi = Person()
+
+    println(eko)
+    println(joko)
+    println(budi)
+}
